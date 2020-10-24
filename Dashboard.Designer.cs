5cc0c0755b83b1ebd792cbbbcde0cde9988fae81
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-10-24 15:22:03 +0800

    added rgb background color

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(84)))), ((int)(((byte)(133)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
